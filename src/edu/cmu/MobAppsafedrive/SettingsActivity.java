7c58e1683f31764a4d4eb2a8c8cc10c895b1e3ba
mownica <mownica1990@gmail.com>
2014-08-02 19:03:37 -0400

    settingsActivity

---
+++ src/edu/cmu/MobAppsafedrive/SettingsActivity.java
@@ -0,0 +1,73 @@
+package edu.cmu.MobAppsafedrive;
+
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+public class SettingsActivity extends ActionBarActivity {
+
+	private SeekBar seekBar1;
+	private SeekBar seekBar2;
+	Spinner spinner;
+	Switch switchValue;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings1);
+		
+		
+	}
+
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+	//	getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+	
+	//public void settingDetails(View view){
+	public void onPause(){
+		
+		super.onPause();
+		System.out.println("Inside settings");
+		
+		seekBar1 = (SeekBar)findViewById(R.id.seekBar1);
+		seekBar2 = (SeekBar)findViewById(R.id.seekBar2);
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		switchValue = (Switch) findViewById(R.id.switch1);
+		
+		String s1 = Integer.toString((seekBar1).getProgress());
+		String s2 = Integer.toString((seekBar2).getProgress());
+		String value = spinner.getSelectedItem().toString();
+		String sV =	Boolean.toString(switchValue.isChecked());
+		
+		
+		System.out.println(s1 +" "+s2+" "+value+" "+sV);
+		
+		
+	}
+	
+	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
